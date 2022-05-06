7404352a55e18fd6b8bc21d3037665e45d2eb8b8
Konstantin <kostyabashkov@gmail.com>
2022-05-06 08:27:43 +0300

    change month icons

--- src/components/HeaderCurrentDate/CurrentDate.vue
+++ src/components/HeaderCurrentDate/CurrentDate.vue
@@ -1,9 +1,17 @@
 <template>
-  <div >
-    <button @click="prevMonth">prev</button>
-    <button @click="nextMonth">next</button>
-    {{ month }}
-    {{ year }}
+  <div class="months">
+    <div class="month-nav">
+      <div class="nav-item" @click="prevMonth">
+        <Icon iconName="angle-left"/>
+      </div>
+      <div class="nav-item" @click="nextMonth">
+        <Icon iconName="angle-right"/>
+      </div>
+    </div>
+    <div class="current-month">
+      {{ month }}
+      {{ year }}
+    </div>
   </div>
 </template>
 
@@ -11,10 +19,9 @@
 import { useStore } from '@/store'
 import {computed} from "vue";
 import moment from "moment";
+import Icon from "@/components/UI/Icon";
 
-
-
-const store = useStore();
+const store = useStore()
 
 const nextMonth = () => store.initialDate = moment(store.initialDate).add(1, 'month')
 const prevMonth = () => store.initialDate = moment(store.initialDate).subtract(1, 'month')
@@ -25,7 +32,36 @@ const year = computed(() => store.initialDate.format('YYYY'))
 </script>
 
 <style lang="scss" scoped>
-div {
+.months {
+  display: flex;
+  align-items: center;
+  margin-left: 16px;
+}
+
+.month-nav {
+  display: flex;
+  cursor: pointer;
+  margin-top: 3px;
+}
+
+.nav-item {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+    background-color: var(--c-block-hover);
+  }
+
+  svg {
+    width: 16px;
+    height: 16px;
+  }
+}
+
+.current-month {
   font-size: 22px;
   font-weight: 500;
   margin-left: 12px;
